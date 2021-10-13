9022b29f841c959e6fefd59cbca6959608afbaa8
Princy <princy_prabhakaran@dellteam.com>
2021-10-13 06:56:10 +0530

    Added Installer3

---
+++ Test3/Installer/InstallationManager/ManagedCustomActions/ManagedCustomActions/Package/SystemDetails.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+namespace PackageAssitant
+{
+    class SystemDetails
+    {
+        public static string GetManufacturer()
+        {
+            string manuf = string.Empty;
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            //collection to store all management objects
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    if (mo["Manufacturer"] != null)
+                    {
+                        manuf = mo["Manufacturer"].ToString();
+                    }
+
+                }
+
+            }
+            return manuf;
+        }
+        public static string GetModel()
+        {
+            string model = string.Empty;
+            ManagementClass mc = new ManagementClass("Win32_ComputerSystem");
+            //collection to store all management objects
+            ManagementObjectCollection moc = mc.GetInstances();
+            if (moc.Count != 0)
+            {
+                foreach (ManagementObject mo in mc.GetInstances())
+                {
+                    // display general system information
+                    if (mo["Model"] != null)
+                    {
+                        model = mo["Model"].ToString();
+                    }
+
+                }
+
+            }
+            return model;
+        }
+    }
+}
