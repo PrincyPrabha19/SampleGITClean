9f09008af59ec4343c439610e3e487465e571bff
Princy <princy_prabhakaran@dellteam.com>
2021-10-13 06:55:20 +0530

    Added installer2

---
+++ Test2/Installer/InstallationManager/InstallationManager.ToastNotification/InstallationManager.DataModel/IToastUpdaterData.cs
@@ -0,0 +1,28 @@
+﻿namespace InstallationManager.DataModel
+{
+    public enum OverallInstallationStatus
+    {
+        Launch,
+        Staging,
+        Downloading,
+        Extracting,
+        Installing,
+        Configuring,
+        Cleanup,
+        Rollback,
+        Repairing,
+        Removing,
+        Updating,
+        Modifying,
+        Completed
+    }
+
+    public interface IToastUpdaterData
+    {
+        string InstallationStatus { get; }
+        double ProgressValue { get; }
+        double ProgressValueNext { get; }
+        string ProgressValueStringOverride { get; }
+        OverallInstallationStatus OInstallStatus { get; }
+    }
+}
