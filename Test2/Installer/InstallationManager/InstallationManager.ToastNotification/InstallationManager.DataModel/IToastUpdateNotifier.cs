e692a3f2b557f60fcb6d7c0c9af768995adcdefd
Princy <princy_prabhakaran@dellteam.com>
2021-10-13 06:55:20 +0530

    Added installer2

---
+++ Test2/Installer/InstallationManager/InstallationManager.ToastNotification/InstallationManager.DataModel/IToastUpdateNotifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace InstallationManager.DataModel
+{
+    public interface IToastUpdateNotifier
+    {
+        event EventHandler<IToastUpdaterData> ToastUpdateReceived;
+
+        void OnToastUpdateReceived();
+
+        void UpdateListener();
+    }
+}
---
+++ Test2/Installer/InstallationManager/InstallationManager.ToastNotification/InstallationManager.ToastNotificationLib/ToastUpdate.cs
@@ -0,0 +1,22 @@
+﻿using InstallationManager.DataModel;
+
+namespace InstallationManager.ToastNotificationLib
+{
+    public class ToastUpdate : IToastUpdaterData
+    {
+        public string InstallationStatus { get; private set; }
+        public double ProgressValue { get; private set; }
+        public string ProgressValueStringOverride { get; private set; }
+        public OverallInstallationStatus OInstallStatus { get; private set; }
+
+        public double ProgressValueNext { get; private set; }
+
+        public ToastUpdate(string InstallationStatus = "Installation Started", double ProgressValue = 0.00d, string ProgressValueStringOverride = "0 %", OverallInstallationStatus OInstallStatus = OverallInstallationStatus.Launch)
+        {
+            this.InstallationStatus = InstallationStatus;
+            this.ProgressValue = ProgressValue;
+            this.ProgressValueStringOverride = ProgressValueStringOverride;
+            this.OInstallStatus = OInstallStatus;
+        }
+    }
+}
